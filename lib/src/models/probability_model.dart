b006411d6c465765223700b974137c1578227305
Douglas R. Ferreira <drfdouglas@outlook.com>
2022-02-02 02:32:07 -0300

    Monte Carlo Simulation Algorithm

---
+++ lib/src/models/probability_model.dart
@@ -0,0 +1,19 @@
+class Probability {
+
+  Probability();
+
+  int scenarios = 0;
+  
+  double winProbability = 0;
+  double tieProbability = 0;
+
+  double probabilityToStraightFlush = 0;
+  double probabilityToFourOfAKind = 0;
+  double probabilityToFullHouse = 0;
+  double probabilityToFlush = 0;
+  double probabilityToStraight = 0;
+  double probabilityToThreeOfAKind = 0;
+  double probabilityToTwoPairs = 0;
+  double probabilityToPair = 0;
+  double probabilityToHighCard = 0;
+}
