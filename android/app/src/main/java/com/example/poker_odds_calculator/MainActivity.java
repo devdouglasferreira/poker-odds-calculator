bfec273932c6bba8614387af7bc91a9cc89b947b
Douglas R. Ferreira <drfdouglas@outlook.com>
2022-02-04 04:29:33 -0300

    Upgrade android project

--- android/app/src/main/java/com/example/poker_odds_calculator/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package br.com.ferreiratech.pokeroddscalculator;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-  
-}
---
+++ android/app/src/main/kotlin/com/example/pokeroddscalculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ferreiratech.pokeroddscalculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
